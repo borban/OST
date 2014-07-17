466bfb647f8e9307752582237c67da06c5ea7eec
borban <benjamin.orban@gmail.com>
2014-07-17 10:31:50 -0500

    Adding javascript work

--- javascript2/todo.js
+++ javascript2/todo.js
@@ -1,4 +1,5 @@
-function Todo(task, who, dueDate){
+function Todo(id, task, who, dueDate){
+  this.id = id;
   this.task = task;
   this.who = who;
   this.dueDate = dueDate;
@@ -12,51 +13,35 @@ window.onload = init;
 function init() {
     var submitButton = document.getElementById("submit");
     submitButton.onclick = getFormData;
-    getTodoData();
+    getTodoItems();
 }
 
-function getTodoData() {
-    var request = new XMLHttpRequest();
-    request.open("GET", "todo.json");
-    request.onreadystatechange = function() {
-        var listDiv = document.getElementById("todoList");
-        if (this.readyState == this.DONE && this.status == 200) {
-            if (this.responseText) { 
-                parseTodoItems(this.responseText);
-                addTodosToPage();
-            }
-            else {
-                console.log("Error: Data is empty");
-            }
-        }
-    };
-    request.send();
-} 
-
-function parseTodoItems(todoJSON) {
-    if (todoJSON == null || todoJSON.trim() == "") {
-        return;
-    }
-    var todoArray = JSON.parse(todoJSON);
-    if (todoArray.length == 0) {
-        console.log("Error: the to-do list array is empty!");
-        return;
-    }
-    for (var i = 0; i < todoArray.length; i++) {
-        var todoItem = todoArray[i];
+function getTodoItems(){
+  if(localStorage){
+    for(var i = 0; i < localStorage.length; i++){
+      var key = localStorage.key(i);
+      if (key.substring(0,4) == "todo"){
+        var item = localStorage.getItem(key);
+        var todoItem = JSON.parse(item);
         todos.push(todoItem);
+      }
     }
-} 
+    addTodosToPage();
+  }
+  else {
+    console.log("Error: you don't have localStorage!");
+  }
+}
 
 function addTodosToPage(){
   var ul = document.getElementById("todoList");
+  var listFragment = document.createDocumentFragment();
   for(var i = 0; i < todos.length; i++){
     var todoItem = todos[i];
-    var li = document.createElement("li");
-    li.innerHTML = 
-      todoItem.who + " needs to " + todoItem.task + " by " + todoItem.dueDate;
-    ul.appendChild(li);
+    var li = createNewTodo(todoItem);
+    listFragment.appendChild(li);
   }
+  ul.appendChild(listFragment);
 }
 
 function getFormData(){
@@ -69,11 +54,11 @@ function getFormData(){
   var date = document.getElementById("dueDate").value;
   if(checkInputText(date, "Please enter a due date")) return;
   
-  console.log("New task: " + task + ", for: " + who + ", by: " + date);
-  var todoItem = new Todo(task, who, date);
+  var id = todos.length;
+  var todoItem = new Todo(id, task, who, date);
   todos.push(todoItem);
   addTodoToPage(todoItem);
-  saveTodoData();
+  saveTodoItem(todoItem);
 }
 
 function checkInputText(value, msg){
@@ -84,21 +69,41 @@ function checkInputText(value, msg){
   return false;
 }
 
+function saveTodoItem(todoItem){
+  if(localStorage){
+    var key = "todo" + todoItem.id;
+    var item = JSON.stringify(todoItem);
+    localStorage.setItem(key, item);
+  }
+  else {
+    console.log("Error: you don't have localStorage!");
+  }
+}
+
 function addTodoToPage(todoItem){
   var ul = document.getElementById("todoList");
-  var li = document.createElement("li");
-  li.innerHTML = 
-    todoItem.who + " needs to " + todoItem.task + " by " + todoItem.dueDate;
+  var li = createNewTodo(todoItem);
   ul.appendChild(li);
   document.forms[0].reset();
 }
 
-function saveTodoData(){
-  var todoJSON = JSON.stringify(todos);
-  var request = new XMLHttpRequest();
-  var URL = "save.php?data=" + encodeURI(todoJSON);
-  request.open("GET", URL);
-  request.setRequestHeader("Content-Type",
-  			   "text/plain;charset=UTF-8");
-  request.send();
+function createNewTodo(todoItem){
+  var li = document.createElement("li");
+  var spanTodo = document.createElement("span");
+  spanTodo.innerHTML = 
+  	todoItem.who + " needs to " + todoItem.task + " by " + todoItem.dueDate;
+  
+  var spanDone = document.createElement("span");
+  if(!todoItem.done){
+    spanDone.setAttribute("class", "notDone");
+    spanDone.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+  }
+  else {
+    spanDone.setAttribute("class", "done");
+    spanDone.innerHTML + "&nbsp;&#10004;&nbsp;";
+  }
+  
+  li.appendChild(spanDone);
+  li.appendChild(spanTodo);
+  return li;
 }
